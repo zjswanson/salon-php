1c2063107da8fabbddd3b8c3eda3c2feec4d8e0a
Zach Swanson <zach.j.swanson@gmail.com>
2017-02-24 11:38:56 -0800

    add tests for client save, deleteall, getall methods

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -12,10 +12,11 @@ $password = 'root';
 $DB = new PDO($server, $username, $password);
 class ClientTest extends PHPUnit_Framework_TestCase
 {
-    // protected function tearDown()
-    // {
-    //     Stylist::deleteAll();
-    // }
+    protected function tearDown()
+    {
+        Stylist::deleteAll();
+        Client::deleteAll();
+    }
     function test_getters()
     {
         // Arrange
@@ -48,6 +49,47 @@ class ClientTest extends PHPUnit_Framework_TestCase
         // Assert
         $this->assertEquals($result, $expected_result);
     }
+
+    function test_Save()
+    {
+        // Arrange
+        $stylist_name = 'Eduardo';
+        $specialty = "pompadour";
+        $test_stylist = new Stylist ($stylist_name,$specialty);
+        $test_stylist->save();
+        $stylist_id = $test_stylist->getId();
+        $client_name = 'Mr. Dude';
+        $next_appointment = "2017-02-24 15:00:00";
+        $test_client = new Client ($client_name,$next_appointment,$stylist_id);
+        $test_client->save();
+        // Act
+        $result = Client::getAll();
+        // Assert
+        $this->assertEquals($result[0], $test_stylist);
+    }
+
+    function test_getAll()
+    {
+        // Arrange
+        $stylist_name = 'Eduardo';
+        $specialty = "pompadour";
+        $test_stylist = new Stylist ($stylist_name,$specialty);
+        $test_stylist->save();
+        $stylist_id = $test_stylist->getId();
+        $client_name = 'Mr. Dude';
+        $next_appointment = "2017-02-24 15:00:00";
+        $test_client = new Client ($client_name,$next_appointment,$stylist_id);
+        $test_client->save();
+        $client_name2 = 'El Duderino';
+        $next_appointment2 = "2017-02-24 17:00:00";
+        $test_client2 = new Client ($client_name2,$next_appointment2,$stylist_id);
+        $test_client2->save();
+        // Act
+        $result = Client::getAll();
+        // Assert
+        $this->assertEquals($result, [$test_client,$test_client2]);
+    }
+
 
 
 
--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -15,6 +15,7 @@ class StylistTest extends PHPUnit_Framework_TestCase
     protected function tearDown()
     {
         Stylist::deleteAll();
+        Client::deleteAll();
     }
     function test_getters()
     {
