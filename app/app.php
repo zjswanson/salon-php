c4bd398cf8a82fa54020b2b48038607f32f115c7
Zach Swanson <zach.j.swanson@gmail.com>
2017-02-24 14:01:55 -0800

    create route and template to display client list for stylists

--- app/app.php
+++ app/app.php
@@ -3,6 +3,7 @@
 
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Stylist.php";
+    require_once __DIR__."/../src/Client.php";
 
     $app = new Silex\Application();
 
@@ -29,6 +30,20 @@
         $new_stylist = new Stylist($_POST['stylist_name'], $_POST['specialty']);
         $new_stylist->save();
         return $app['twig']->render('stylist_list.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->get("/clients/{stylist_id}", function($stylist_id) use ($app) {
+        $stylist = Stylist::find($stylist_id);
+        $clients = $stylist->findClients();
+        return $app['twig']->render('client_list.html.twig', array('stylist' => $stylist, 'clients' => $clients));
+    });
+
+    $app->post("/clients/{stylist_id}", function($stylist_id) use ($app) {
+        $stylist = Stylist::find($stylist_id);
+        $new_client = new Client($_POST['client_name'],$_POST['appointment'],$stylist->getId());
+        $new_client->save();
+        $clients = $stylist->findClients();
+        return $app['twig']->render('client_list.html.twig', array('stylist' => $stylist, 'clients' => $clients));
     });
 
 
