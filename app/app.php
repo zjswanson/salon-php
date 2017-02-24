05544644a43597579d29307a4fdf08787b2b7bf9
Zach Swanson <zach.j.swanson@gmail.com>
2017-02-24 15:47:13 -0800

    add patch and delete routes for clients

--- app/app.php
+++ app/app.php
@@ -42,6 +42,20 @@
         return $app['twig']->render('stylist_list.html.twig', array('stylists' => Stylist::getAll()));
     });
 
+
+    $app->get("/stylist_edit/{stylist_id}", function($stylist_id) use ($app) {
+        return $app['twig']->render('stylist_edit.html.twig', array('stylist' => Stylist::find($stylist_id)));
+    });
+
+    $app->patch("/stylist_edit/{stylist_id}", function($stylist_id) use ($app) {
+        $stylist = Stylist::find($stylist_id);
+        if (!empty($_POST['stylist_name']))
+        {$stylist->update('stylist_name',$_POST['stylist_name']);}
+        if (!empty($_POST['specialty']))
+        {$stylist->update('specialty',$_POST['specialty']);}
+        return $app['twig']->render('stylist_edit.html.twig', array('stylist' => Stylist::find($stylist_id)));
+    });
+
     $app->get("/clients/{stylist_id}", function($stylist_id) use ($app) {
         $stylist = Stylist::find($stylist_id);
         $clients = $stylist->findClients();
@@ -56,23 +70,54 @@
         return $app['twig']->render('client_list.html.twig', array('stylist' => $stylist, 'clients' => $clients));
     });
 
-    $app->get("/stylist_edit/{stylist_id}", function($stylist_id) use ($app) {
-        return $app['twig']->render('stylist_edit.html.twig', array('stylist' => Stylist::find($stylist_id)));
+    // placeholder for route from deleting a client
+    $app->delete("/delete_client/{client_id}", function($client_id) use ($app) {
+        $new_client = Client::find($client_id);
+        $stylist = Stylist::find($new_client->getStylistId());
+        $new_client->delete();
+        $clients = $stylist->findClients();
+        return $app['twig']->render('client_list.html.twig', array('stylist' => $stylist, 'clients' => $clients));
     });
 
-    $app->patch("/stylist_edit/{stylist_id}", function($stylist_id) use ($app) {
-        $stylist = Stylist::find($stylist_id);
-        if (!empty($_POST['stylist_name']))
-        {$stylist->update('stylist_name',$_POST['stylist_name']);}
-        $stylist = Stylist::find($stylist_id);
-        if (!empty($_POST['specialty']))
-        {$stylist->update('specialty',$_POST['specialty']);}
-        return $app['twig']->render('stylist_edit.html.twig', array('stylist' => Stylist::find($stylist_id)));
+    $app->get("/client_edit/{client_id}", function($client_id) use ($app) {
+        $client = Client::find($client_id);
+        $stylist = Stylist::find($client->getStylistId());
+        return $app['twig']->render('client_edit.html.twig', array('client' => Client::find($client_id),'stylist' => $stylist));
+    });
+
+    $app->patch("/client_edit/{client_id}", function($client_id) use ($app) {
+        $client = Client::find($client_id);
+        if (!empty($_POST['client_name']))
+        {$client->update('client_name',$_POST['client_name']);}
+        if (!empty($_POST['next_appointment']))
+        {$client->update('next_appointment',$_POST['next_appointment']);}
+        $stylist = Stylist::find($client->getStylistId());
+        return $app['twig']->render('client_edit.html.twig', array('client' => Client::find($client_id),'stylist' => $stylist));
     });
 
 
-    // This route is just here to auto populate all stylists with a few clients for UI testing.
-    $app->get("/populate", function() use ($app) {
+    // These routes are just here to auto populate all stylists with a few clients for UI testing.
+    $app->get("/populate_stylists", function() use ($app) {
+        $stylist_name = 'Eduardo';
+        $specialty = "pompadour";
+        $test_stylist = new Stylist ($stylist_name,$specialty);
+        $test_stylist->save();
+        $stylist_name2 = 'Phillipe';
+        $specialty2 = "Wavy Mess";
+        $test_stylist2 = new Stylist ($stylist_name2,$specialty2);
+        $test_stylist2->save();
+        $stylist_name3 = 'Meekus';
+        $specialty3 = "Styling Gel";
+        $test_stylist3 = new Stylist ($stylist_name3,$specialty3);
+        $test_stylist3->save();
+        $stylist_name4 = 'Brint';
+        $specialty4 = "Male Models";
+        $test_stylist4 = new Stylist ($stylist_name4,$specialty4);
+        $test_stylist4->save();
+        return $app['twig']->render('stylist_list.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->get("/populate_clients", function() use ($app) {
         $stylists = Stylist::getAll();
         foreach ($stylists as $stylist)
         {
